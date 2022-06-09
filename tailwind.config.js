9f5e8f00f4e7a3b1e5cd85ff07b444f1c606a754
BlackBros <the.blackkbros@gmail.com>
2022-06-09 23:34:45 +0100

    new commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'public/*.html',
+  ],
+  theme: {
+    extend: {},
+    fontFamily:
+    {
+      vcr: "'VCR OSD Mono', sans-serif"
+    }
+  },
+  plugins: [
+    require('@tailwindcss/typography')
+  ],
+}
